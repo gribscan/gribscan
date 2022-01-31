8a4d8f3074ceda3f84611314b2a00742589bd89d
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2022-01-31 20:08:32 +0100

    include forecast time for grib message time calculation

--- gribscan.py
+++ gribscan.py
@@ -37,19 +37,124 @@ def _split_file(f, skip=0):
         if skip and part > skip:
             break
 
+EXTRA_PARAMETERS = [
+    "forecastTime",
+    "indicatorOfUnitOfTimeRange",
+    "lengthOfTimeRange",
+    "indicatorOfUnitForTimeRange",
+    "productDefinitionTemplateNumber",
+]
+
+production_template_numbers = {
+    0: {"forcastTime": True, "timeRange": False},
+    1: {"forcastTime": True, "timeRange": False},
+    2: {"forcastTime": True, "timeRange": False},
+    3: {"forcastTime": True, "timeRange": False},
+    4: {"forcastTime": True, "timeRange": False},
+    5: {"forcastTime": True, "timeRange": False},
+    6: {"forcastTime": True, "timeRange": False},
+    7: {"forcastTime": True, "timeRange": False},
+    15: {"forcastTime": True, "timeRange": False},
+    32: {"forcastTime": True, "timeRange": False},
+    33: {"forcastTime": True, "timeRange": False},
+    40: {"forcastTime": True, "timeRange": False},
+    41: {"forcastTime": True, "timeRange": False},
+    44: {"forcastTime": True, "timeRange": False},
+    45: {"forcastTime": True, "timeRange": False},
+    48: {"forcastTime": True, "timeRange": False},
+    51: {"forcastTime": True, "timeRange": False},
+    53: {"forcastTime": True, "timeRange": False},
+    54: {"forcastTime": True, "timeRange": False},
+    55: {"forcastTime": True, "timeRange": False},
+    56: {"forcastTime": True, "timeRange": False},
+    57: {"forcastTime": True, "timeRange": False},
+    58: {"forcastTime": True, "timeRange": False},
+    60: {"forcastTime": True, "timeRange": False},
+    1000: {"forcastTime": True, "timeRange": False},
+    1002: {"forcastTime": True, "timeRange": False},
+    1100: {"forcastTime": True, "timeRange": False},
+    40033: {"forcastTime": True, "timeRange": False},
+    40455: {"forcastTime": True, "timeRange": False},
+    40456: {"forcastTime": True, "timeRange": False},
+
+    20: {"forcastTime": False, "timeRange": False},
+    30: {"forcastTime": False, "timeRange": False},
+    31: {"forcastTime": False, "timeRange": False},
+    254: {"forcastTime": False, "timeRange": False},
+    311: {"forcastTime": False, "timeRange": False},
+    2000: {"forcastTime": False, "timeRange": False},
+
+    8: {"forcastTime": True, "timeRange": True},
+    9: {"forcastTime": True, "timeRange": True},
+    10: {"forcastTime": True, "timeRange": True},
+    11: {"forcastTime": True, "timeRange": True},
+    12: {"forcastTime": True, "timeRange": True},
+    13: {"forcastTime": True, "timeRange": True},
+    14: {"forcastTime": True, "timeRange": True},
+    34: {"forcastTime": True, "timeRange": True},
+    42: {"forcastTime": True, "timeRange": True},
+    43: {"forcastTime": True, "timeRange": True},
+    46: {"forcastTime": True, "timeRange": True},
+    47: {"forcastTime": True, "timeRange": True},
+    61: {"forcastTime": True, "timeRange": True},
+    67: {"forcastTime": True, "timeRange": True},
+    68: {"forcastTime": True, "timeRange": True},
+    91: {"forcastTime": True, "timeRange": True},
+    1001: {"forcastTime": True, "timeRange": True},
+    1101: {"forcastTime": True, "timeRange": True},
+    10034: {"forcastTime": True, "timeRange": True},
+}
+
+# according to http://www.cosmo-model.org/content/consortium/generalMeetings/general2014/wg6-pompa/grib2/grib/pdtemplate_4.41.htm
+time_range_units = {
+    0: 60,  # np.timedelta64(1, "m"),
+    1: 60*60,  # np.timedelta64(1, "h"),
+    2: 24*60*60,  # np.timedelta64(1, "D"),
+    #3   Month
+    #4   Year
+    #5   Decade (10 years)
+    #6   Normal (30 years)
+    #7   Century (100 years)
+    #8-9 Reserved
+    10: 3*60*60,  # np.timedelta64(3, "h"),
+    11: 6*60*60,  # np.timedelta64(6, "h"),
+    12: 12*60*60,  # np.timedelta64(12, "h"),
+    13: 1,  # np.timedelta64(1, "s"),
+    #14-191  Reserved
+    #192-254 Reserved for local use
+    #255 Missing
+}
+
+def get_time_offset(gribmessage):
+    offset = 0  # np.timedelta64(0, "s")
+    try:
+        options = production_template_numbers[int(gribmessage["productDefinitionTemplateNumber"])]
+    except KeyError:
+        return offset
+    if options["forcastTime"]:
+        unit = time_range_units[int(gribmessage.get("indicatorOfUnitOfTimeRange", 255))]
+        offset += gribmessage.get("forecastTime", 0) * unit
+    # TODO: handling of time ranges, see cdo: libcdi/src/gribapi_utilities.c: gribMakeTimeString
+    return offset
+
 def gribscan(filelike):
     for offset, size, data in _split_file(filelike):
         m = cfgrib.cfmessage.CfMessage(eccodes.codes_new_from_message(data))
         t = eccodes.codes_get_native_type(m.codes_id, "values")
         s = eccodes.codes_get_size(m.codes_id, "values")
+
         yield {"globals": {k: m[k] for k in cfgrib.dataset.GLOBAL_ATTRIBUTES_KEYS},
                "attrs": {k: m.get(k, None) for k in cfgrib.dataset.DATA_ATTRIBUTES_KEYS + cfgrib.dataset.EXTRA_DATA_ATTRIBUTES_KEYS},
-               "time": m["time"],
+               "time": m["time"] + get_time_offset(m),
+               "referenceTime": m["time"],
+               "step": m["step"],
                "offset": offset,
                "size": size,
                "array": {
                    "dtype": np.dtype(t).str,
-                   "shape": [s]},
+                   "shape": [s],
+                },
+               "extra": {k: m.get(k, None) for k in EXTRA_PARAMETERS},
                }
 
 def raise_duplicate_error(a, b, context):
