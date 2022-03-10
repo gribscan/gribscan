f9d3df83c22a7b38185039d4d4c774f6db4eaa37
Lukas Kluft <lukas.kluft@gmail.com>
2022-03-10 09:51:11 +0100

    fixup! Add command-line interface

---
+++ gribscan/tools.py
@@ -0,0 +1,31 @@
+import argparse
+import json
+from pathlib import Path
+import multiprocessing as mp
+
+import gribscan
+
+
+def create_index():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("sources", metavar="GRIB", help="source gribfile(s)", nargs="+")
+    parser.add_argument("-n", "--nprocs", help="number of parallel processes", type=int, default=1, nargs="?")
+    args = parser.parse_args()
+
+    with mp.Pool(args.nprocs) as pool:
+        pool.map(gribscan.write_index, args.sources)
+
+
+def build_dataset():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("indices", metavar="GRIB.index", help="source index files (JSONLines)", nargs="+")
+    parser.add_argument("output", metavar="refs.json", help="reference filesystem specification (JSON)")
+    args = parser.parse_args()
+
+    metadata = gribscan.parse_indices(args.indices)
+    refs = gribscan.grib2kerchunk_refs(metadata)
+
+    refs["templates"]["u"] = Path(args.output).parent.resolve().as_posix() + "/"
+
+    with open(args.output, "w") as indexfile:
+        json.dump(refs, indexfile)
