6e3e577aa8f60437f1d76da466b43123eae8ad87
Lukas Kluft <lukas.kluft@gmail.com>
2022-05-10 10:09:40 +0200

    Bump version before release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="gribscan",
-    version="0.0.2",
+    version="0.0.3",
     description="create indices for GRIB files and provide an xarray interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
