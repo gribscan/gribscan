95e57d083816950a8fb6eced3872f91b9cae8ea1
Leif Denby <leif@denby.eu>
2023-09-19 10:03:38 +0200

    add missing deg2rad

--- gribscan/rotated_grid.py
+++ gribscan/rotated_grid.py
@@ -37,11 +37,10 @@ def rot_to_reg(pole_lon, pole_lat, lon, lat):
 
     lat2 = np.rad2deg(np.arcsin(lat_tmp))
 
-    cos_lat2 = np.cos(lat2 * rad)
+    cos_lat2 = np.cos(np.deg2rad(lat2))
 
     cos_tmp = (cos_pole * cos_lat * cos_lon - sin_pole * sin_lat) / cos_lat2
-
-        cos_tmp = np.clip(cos_tmp, -1.0, 1.0)
+    cos_tmp = np.clip(cos_tmp, -1.0, 1.0)
 
     tmp_sin_lon = cos_lat * sin_lon / cos_lat2
     tmp_cos_lon = np.rad2deg(np.arccos(cos_tmp))
@@ -51,76 +50,3 @@ def rot_to_reg(pole_lon, pole_lat, lon, lat):
     lon2 = tmp_cos_lon + pole_lon
 
     return lon2, lat2
-
-
-def get_gids(gribfile: str, TextIOWrapper: bool = False) -> list:
-    """Get GribIDs (gid) for all the messages in one gribfile
-
-    Parameters
-    ----------
-    gribfile : str
-        path to gribfile
-    TextIOWrapper : bool
-        if file is open send the object instead and set TextIOWrapper to True
-
-    Returns
-    -------
-    list
-        list of grib-ids
-    """
-
-    if not TextIOWrapper:
-        f = open(gribfile, "rb")
-        msg_count = ec.codes_count_in_file(f)
-        gids = [ec.codes_grib_new_from_file(f) for i in range(msg_count)]
-        f.close()
-    else:
-        # gribfile has already been opened into a TextIOWrapper in this case
-        msg_count = ec.codes_count_in_file(gribfile)
-        gids = np.zeros(msg_count, dtype=int)
-        for i in range(msg_count):
-            gids[i] = ec.codes_grib_new_from_file(gribfile)
-
-    return gids
-
-
-def get_latlons(gribfile: str) -> tuple:
-    """Get latitudes and longitudes from file. Uses pygrib as
-    eccodes have no easy interface for that.
-
-    Parameters
-    ----------
-    gribfile : str
-        Path to gribfile
-
-    Returns
-    -------
-    tuple
-        tuple of latitudes, longitudes
-    """
-
-    gids = get_gids(gribfile)
-    gid = gids[0]
-    Ni = ec.codes_get(gid, "Ni")
-    Nj = ec.codes_get(gid, "Nj")
-
-    latFirst = ec.codes_get(gid, "latitudeOfFirstGridPointInDegrees")
-    latLast = ec.codes_get(gid, "latitudeOfLastGridPointInDegrees")
-    lonFirst = ec.codes_get(gid, "longitudeOfFirstGridPointInDegrees")
-    lonLast = ec.codes_get(gid, "longitudeOfLastGridPointInDegrees")
-
-    latPole = ec.codes_get(gid, "latitudeOfSouthernPoleInDegrees")
-    lonPole = ec.codes_get(gid, "longitudeOfSouthernPoleInDegrees")
-
-    gridType = ec.codes_get(gid, "gridType")
-
-    lons, lats = np.meshgrid(
-        np.linspace(lonFirst, lonLast, Ni), np.linspace(latFirst, latLast, Nj)
-    )
-
-    if gridType == "rotated_ll":
-        lons, lats = rot_to_reg(lonPole, latPole, lons, lats)
-    else:
-        raise NotImplementedError(gridType)
-
-    return lats, lons
