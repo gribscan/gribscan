80d512c0b51a11d6c250c0bb9d6430f8094b1358
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2024-02-06 15:41:37 +0100

    move from setup.py to pyproject.toml and activate setuptools-scm

--- gribscan/__init__.py
+++ gribscan/__init__.py
@@ -2,4 +2,11 @@ from gribscan.gribscan import *
 from gribscan.aeccodec import *
 from gribscan.rawgribcodec import *
 
-__version__ = "0.0.8"
+
+try:
+    # the ._version module should be created by setuptools_scm
+    from ._version import __version__, __version_tuple__
+except ImportError:
+    # fallback if it's not there (e.g. package not properly installed)
+    __version__ = "unknown"
+    __version__tuple__ = ()
