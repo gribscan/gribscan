4901da1caf83c94259876d67b5571acb3de12a02
Lukas Kluft <lukas.kluft@gmail.com>
2025-09-08 22:08:52 +0200

    Create new `gribscan` CLI and use click to handle options

--- gribscan/tools.py
+++ gribscan/tools.py
@@ -1,4 +1,3 @@
-import argparse
 import glob
 import json
 import logging
@@ -7,136 +6,109 @@ from functools import partial
 from pathlib import Path
 import multiprocessing as mp
 
+import click
+
 import gribscan
 from .magician import MAGICIANS
 
 
-def create_index():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("sources", metavar="GRIB", help="source gribfile(s)", nargs="+")
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        help="increase the logging level",
-        action="store_true",
-    )
-    parser.add_argument(
-        "-o",
-        "--outdir",
-        help="output directory to write index files",
-        type=str,
-        default=None,
-        nargs="?",
-    )
-    parser.add_argument(
-        "-f",
-        "--force",
-        help="overwrite existing index files",
-        action="store_true",
-    )
-    parser.add_argument(
-        "-n",
-        "--nprocs",
-        help="number of parallel processes",
-        type=int,
-        default=1,
-        nargs="?",
-    )
-    args = parser.parse_args()
-
+@click.group()
+@click.option("-v", "--verbose", is_flag=True, help="Increase the logging level.")
+def cli(verbose):
+    """gribscan: Index and build GRIB datasets."""
     logging.basicConfig()
-    if args.verbose:
+    if verbose:
         logging.getLogger().setLevel(logging.DEBUG)
 
-    mapfunc = partial(gribscan.write_index, outdir=args.outdir, force=args.force)
-    with mp.Pool(args.nprocs) as pool:
-        pool.map(mapfunc, args.sources)
+
+@cli.command("index")
+@click.argument("sources", nargs=-1, type=click.Path(exists=True))
+@click.option(
+    "-o",
+    "--outdir",
+    type=click.Path(file_okay=False, writable=True),
+    default=None,
+    help="Output directory to write index files.",
+)
+@click.option("-f", "--force", is_flag=True, help="Overwrite existing index files.")
+@click.option(
+    "-n",
+    "--nprocs",
+    type=int,
+    default=1,
+    show_default=True,
+    help="Number of parallel processes.",
+)
+def create_index(sources, outdir, force, nprocs):
+    """Create index files from GRIB sources."""
+    mapfunc = partial(gribscan.write_index, outdir=outdir, force=force)
+    with mp.Pool(nprocs) as pool:
+        pool.map(mapfunc, sources)
 
 
-def build_dataset():
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawTextHelpFormatter,
-    )
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        help="increase the logging level",
-        action="store_true",
-    )
-    parser.add_argument(
-        "indices",
-        metavar="GRIB.index",
-        help="source index files (JSONLines)",
-        nargs="*",
-    )
-    parser.add_argument(
-        "-g",
-        "--glob",
-        metavar="GLOB.index",
-        help="glob pattern to create list of index files (JSONLines)",
-        type=str,
-        nargs="?",
-        default=None,
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        metavar="outdir",
-        default=".",
-        help="output directory to write reference filesystems (JSON)",
-        type=str,
-    )
-    parser.add_argument(
-        "--prefix",
-        metavar="template_prefix",
-        default=None,
-        help=textwrap.dedent("""\
-            Absolute path to the location of the dataset.
+@cli.command("build")
+@click.argument("indices", nargs=-1, type=click.Path(exists=True))
+@click.option(
+    "-g",
+    "--glob",
+    "glob_pattern",
+    type=str,
+    help="Glob pattern to create list of index files (JSONLines).",
+)
+@click.option(
+    "-o",
+    "--output",
+    type=click.Path(file_okay=False, writable=True),
+    default=".",
+    show_default=True,
+    help="Output directory to write reference filesystems (JSON).",
+)
+@click.option(
+    "--prefix",
+    type=str,
+    help=textwrap.dedent(
+        """\
+        Absolute path to the location of the dataset.
 
-            The prefix is prepended to the filename stored in the index files.
-            For full file paths, a sub-tree can be denoted using the '/./'
-            character. The following examples show how a prefix adds to
-            different filenames:
+        The prefix is prepended to the filename stored in the index files.
+        For full file paths, a sub-tree can be denoted using the '/./'
+        character. The following examples show how a prefix adds to
+        different filenames:
 
             /prefix/ + filename.grb = /prefix/filename.grb
             /prefix/ + path/filename.grb = /prefix/path/filename.grb
             /prefix/ + path/./sub/tree/filename.grb = /prefix/sub/tree/filename.grb
         """
-        ),
-        type=str,
-    )
-    parser.add_argument(
-        "-m",
-        "--magician",
-        metavar="magician",
-        default="monsoon",
-        help="use this magician for dataset assembly",
-        type=str,
-    )
-    args = parser.parse_args()
+    ),
+)
+@click.option(
+    "-m",
+    "--magician",
+    default="monsoon",
+    show_default=True,
+    type=click.Choice(MAGICIANS.keys()),
+    help="Magician to use for dataset assembly.",
+)
+def build_dataset(indices, glob_pattern, output, prefix, magician):
+    """Build dataset references from index files."""
+    if not glob_pattern and not indices:
+        raise click.UsageError("You must provide either a glob pattern or a file list.")
+    if glob_pattern and indices:
+        raise click.UsageError("Cannot provide both glob pattern and file list.")
 
-    logging.basicConfig()
-    if args.verbose:
-        logging.getLogger().setLevel(logging.DEBUG)
+    if glob_pattern:
+        indices = list(glob.iglob(glob_pattern))
 
-    magician = MAGICIANS[args.magician]()
-
-    if args.glob is None and not args.indices:
-        parser.error("You need to pass a glob pattern or a file list.")
-
-    if args.glob is not None and args.indices:
-        parser.error("It is not possible to pass a glob pattern and a file list.")
-
-    if args.glob is not None:
-        indices = glob.iglob(args.glob)
-
-    if args.indices:
-        indices = args.indices
-
+    magician_instance = MAGICIANS[magician]()
     refs = gribscan.grib_magic(
-        indices, magician=magician, global_prefix=args.prefix
+        indices, magician=magician_instance, global_prefix=prefix
     )
 
+    Path(output).mkdir(parents=True, exist_ok=True)
     for dataset, ref in refs.items():
-        with open(f"{args.output}/{dataset}.json", "w") as indexfile:
-            json.dump(ref, indexfile)
+        with open(Path(output) / f"{dataset}.json", "w") as f:
+            json.dump(ref, f, indent=2)
+
+
+if __name__ == "__main__":
+    cli()
